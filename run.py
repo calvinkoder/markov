3fa999aca15eae2341374ec1ccb20c490c10267e
Calvin Koder <calvinkoder@protonmail.com>
2016-11-08 22:23:38 -0500

    Added more functions to run, improved variable names

--- run.py
+++ run.py
@@ -6,6 +6,8 @@ import random
 import time
 from markov import Group, Model
 
+CONFIG_FILE = None
+
 def loop(model, delay = 0, timeout = 0, maxlen = 0, output_file = ''):
 	group = model.random_group()
 	start = time.time()
@@ -14,7 +16,7 @@ def loop(model, delay = 0, timeout = 0, maxlen = 0, output_file = ''):
 
 	count = 0
 
-	while (args.timeout == 0 or time.time() - start < args.timeout) and count <= maxlen:
+	while (timeout == 0 or time.time() - start < timeout) and count <= maxlen:
 		group = model.walk(group)
 
 		if not group:
@@ -33,63 +35,62 @@ def loop(model, delay = 0, timeout = 0, maxlen = 0, output_file = ''):
 
 	print()
 
-def run(input_file, level = 1, use_model = False, output_text = '', save_model = '', merge = '', delay = 0, timeout = 0, maxlen = 0):
-	settings = {}
-	output_file = ''
-	model_file = ''
-	merge_model_file = ''
-	data = None
-	model = None
-	merge_model = None
-
-	with open('config.json', 'r') as f:
-		settings = json.load(f)
-
-	if use_model:
-		input_file = os.path.join(settings['model_path'], input_file)
-	else:
-		input_file = os.path.join(settings['input_path'], input_file)
-	if output_text:
-		output_file = os.path.join(settings['output_path'], output_text)
-	if save_model:
-		model_file = os.path.join(settings['model_path'], save_model)
-	if merge:
-		merge_model_file = os.path.join(settings['model_path'], merge)
-
-		with open(merge_model_file, 'r') as f:
-			merge_model = Model(json.load(f))
-
-	with open(input_file, 'r') as f:
-		#treat as JSON
-		if use_model:
-			data = json.load(f)
-
-		#treat as text
-		else:
-			data=f.read()
-			data=Group(data.split(' '))
-
+def run(data, output_file = '', output_as_model = False, merge_model = None, level = 1,  delay = 0, timeout = 0, maxlen = 0):
 	model = Model(data, level)
 
-	if merge:
+	if merge_model:
 		model += merge_model
 
 	#Save model to file
-	if save_model:
-		with open(model_file, 'w') as f:
+	if output_as_model:
+		with open(output_file, 'w') as f:
 			json.dump(model.__dict__(), f)
 
 	#run model indefinitely, print output
 	else:
 		loop(model, delay = delay, timeout = timeout, maxlen = maxlen, output_file = output_file)
 
+def load_input(input_file, input_as_model = False):
+	with open(input_file, 'r') as f:
+		#treat as JSON
+		if input_as_model:
+			return json.load(f)
+
+		#treat as text
+		else:
+			data = f.read()
+			return Group(data.split(' '))
+
+def main(input_file, settings = {}, input_as_model = False, output_file = '', output_as_model = False, merge_model_file = '', level = 1, delay = 0, timeout = 0, maxlen = 0):
+	data = None
+	merge_model = None
+
+	if input_as_model:
+		input_file = os.path.join(settings.get('model_path', ''), input_file)
+	else:
+		input_file = os.path.join(settings.get('input_path', ''), input_file)
+	if output_as_model:
+		model_file = os.path.join(settings.get('model_path', ''), output_file)
+	elif output_file:
+		output_file = os.path.join(settings.get('output_path', ''), output_file)
+	if merge_model_file:
+		merge_model_file = os.path.join(settings.get('model_path', ''), merge)
+
+		with open(merge_model_file, 'r') as f:
+			merge_model = Model(json.load(f))
+
+	data = load_input(input_file)
+	run(data, output_file, output_as_model, merge_model, level, delay, timeout, maxlen)
+
 if __name__ == '__main__':
+	CONFIG_FILE = 'config.json'
+
 	parser = argparse.ArgumentParser()
-	parser.add_argument('input_file', metavar = 'input_file', help = 'File with input data to be parsed')
+	parser.add_argument('input_file', help = 'File with input data to be parsed')
 	parser.add_argument('-m', action = 'store_true', help = 'Consider input file to be an existing model in JSON')
-	parser.add_argument('-s', default = '', metavar = 'save_model', help = 'Filename for JSON model to be saved to')
-	parser.add_argument('-o', default = '', metavar = 'output_text', help = 'File to output text to')
-	parser.add_argument('-l', default = 1, type = int, metavar='level', help = 'Specify the level of the model (How many nodes per data group)')
+	parser.add_argument('-s', default = '', help = 'File for JSON model to be saved to')
+	parser.add_argument('-o', default = '', help = 'File to output text to')
+	parser.add_argument('-l', default = 1, type = int, help = 'Specify the level of the model (How many nodes per data group)')
 	parser.add_argument('--delay', default = 0, type = float, help = 'time delay between print statements, in milliseconds')
 	parser.add_argument('--timeout', default = 0, type = float, help = 'maximum running duration')
 	parser.add_argument('--maxlen', default = 0, type = int, help = 'Maximum number of words in output')
@@ -99,9 +100,22 @@ if __name__ == '__main__':
 
 	kwargs = vars(args)
 
-	name_subs = {'m':'use_model', 's': 'save_model', 'o':'output_text', 'l': 'level'}
+	name_subs = {'m':'input_as_model', 'l': 'level', 'merge': 'merge_model_file'}
 	for name, sub in name_subs.items():
 		kwargs[sub] = kwargs[name]
 		del kwargs[name]
 
-	run(**kwargs)
+	if args.s:
+		kwargs['output_file'] = args.s
+		kwargs['output_as_model'] = True
+	else:
+		kwargs['output_file'] = args.o
+		kwargs['output_as_model'] = False
+
+	del kwargs['s']
+	del kwargs['o']
+
+	with open(CONFIG_FILE, 'r') as f:
+		kwargs['settings'] = json.load(f)
+
+	main(**kwargs)
