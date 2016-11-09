2a69b068679b8700f69f0ec04e4e0fb09974084c
Calvin Koder <calvinkoder@protonmail.com>
2016-11-08 21:27:57 -0500

    added support for empty model

--- markov.py
+++ markov.py
@@ -28,12 +28,13 @@ class Model:
 	def __init__(self, data, level = 1):
 		self.level = level
 
-		if type(data) == dict:
-			self.model = data.get('model', {})
-			self.level = data.get('level', level)
-			self.node_count = data.get('node_count', {})
-		else:
-			self.generate(data, level)
+		if data:
+			if type(data) == dict:
+				self.model = data.get('model', {})
+				self.level = data.get('level', level)
+				self.node_count = data.get('node_count', {})
+			else:
+				self.generate(data, level)
 
 	def __dict__(self):
 		return {'model': self.model, 'node_count': self.node_count, 'level': self.level}
@@ -71,7 +72,6 @@ class Model:
 		new_model = Model({}, self.level)
 
 		if self.level == _model.level:
-			print('model 0')
 			for group_name, group in self.model.items():
 				self_count = self.node_count[group_name]
 				total_count = self_count
@@ -80,9 +80,6 @@ class Model:
 				if _model.model.get(group_name, None):
 					_model_count = _model.node_count[group_name]
 					total_count += _model_count
-
-				print('group:', group_name)
-				print('count 0: {0}, count 1: {1}, count total: {2}'.format(self_count, _model_count, total_count))
 
 				for node, self_path_val in group.items():
 
@@ -101,26 +98,20 @@ class Model:
 					else:
 						new_model.model[group_name][node] = self_path_val
 
-					print('node: {0}: {1}'.format(node, new_model.model[group_name][node]))
-
 					new_model.node_count[group_name] = total_count
 
-			print('model 1')
 			#add remaining items exclusively in _model
 			for group_name, group in _model.model.items():
-				print('group:', group_name)
 				for node, path_val in group.items():
 					#common group
 					if self.model.get(group_name, None):
 						#exclusive node
 						if not node in self.model.get(group_name, {}):
 							new_model.model[group_name][node] = path_val*_model.node_count[group_name]/new_model.node_count[group_name]
-							print('node: {0}: {1}'.format(node, new_model.model[group_name][node]))
 					#exclusive group
 					else:
 						new_model.add_group(group_name)
 						new_model.model[group_name][node] = path_val
-						print('node: {0}: {1}'.format(node, new_model.model[group_name][node]))
 
 		return new_model
 
